f8e4a226401ef23dfccf8eaa6dc21dd253aa5f69
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-01-30 00:19:32 +0200

    task-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
 
+import throttle  from "lodash.throttle";
+
+const form = document.querySelector(".feedback-form");
+const input = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const stringFormData = localStorage.getItem(LOCALSTORAGE_KEY);
+let parsedFormData = JSON.parse(stringFormData) || {};
+inputValue();
+
+form.addEventListener("submit", saveValue);
+form.addEventListener('input', throttle(formInput, 500));
+
+function formInput(event) {
+  parsedFormData[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(parsedFormData));
+}
+
+function saveValue(event) {
+  event.preventDefault();
+
+  if (input.value === '' || textarea.value === '') {
+    alert('Всі поля повинні бути заповнені!');
+  } else {
+    event.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    console.log("parsedFormData", parsedFormData);
+    parsedFormData = {};
+  }
+}
+
+function inputValue() {
+  
+  if (parsedFormData) {
+    input.value = parsedFormData.email || "";
+    textarea.value = parsedFormData.message || "";
+  }
+}
+
+
