bbdd339212bce1eb835160ccca5057c865976e75
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-01-30 00:18:59 +0200

    task-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import VimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const time = localStorage.getItem(LOCALSTORAGE_KEY);
+const parsTime = JSON.parse(time) || { seconds: 0 };
+
+console.log("time:", parsTime);
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+function onTimeupdate(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+}
+
+player.setCurrentTime(parsTime.seconds).catch(function(error) {
+  console.log('error',error);
+});
 
